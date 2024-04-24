2035c7a1459499d9015bd3b5bb4584338e86db1e
whitlad <50044237@serc.ac.uk>
2024-04-24 10:07:52 +0100

    1st commit

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+
+/**
+
+ * Person details
+ * Represents a director or Actor in a movie
+ *
+
+ * @author Dawn Whitla
+
+ *@version 24/04/2024
+
+ **/
+
+
+public class Person {
+
+    private String name;
+    private String nationality;
+    private LocalDate DOB;
+
+    //CONSTRUCTOR
+
+    /** Constructor to create an object of the person class
+
+}
